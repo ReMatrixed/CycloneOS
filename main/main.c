cfdd5aba3eac7aaa9b9d21f552edb5f48f62d4f8
ReMatrixed <mat.vasenin@yandex.ru>
2024-10-15 16:57:59 +0300

    Добавлен код мастер-службы

--- main/main.c
+++ main/main.c
@@ -1,6 +1,17 @@
+// Внутренние зависимости
 #include <stdio.h>
+#include <esp_log.h>
+#include <esp_err.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+// Мастер-служба. Необходима для запуска других служб и их управления
+void master_task(void *pvParameters)
+{
+
+}
 
 void app_main(void)
 {
-
+    xTaskCreate(master_task, "master_task", 1024, NULL, 5, NULL);
 }
