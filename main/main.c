559b5ce64fd126ff162976e856f6f9426e661a39
ReMatrixed <mat.vasenin@yandex.ru>
2024-10-18 22:56:44 +0300

    Обновлен main.c

--- main/main.c
+++ main/main.c
@@ -5,13 +5,19 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
-// Мастер-служба. Необходима для запуска других служб и их управления
+char* MASTER_TASK_TAG = "master_task";
+
+// Мастер-служба. Необходима для запуска других служб и управления ими
 void master_task(void *pvParameters)
 {
-
+    while(1)
+    {
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        ESP_LOGI(MASTER_TASK_TAG, "Hello from the MasterTask! New era is coming!");
+    }
 }
 
 void app_main(void)
 {
-    xTaskCreate(master_task, "master_task", 1024, NULL, 5, NULL);
+    xTaskCreate(master_task, MASTER_TASK_TAG, 2048, NULL, 5, NULL);
 }
